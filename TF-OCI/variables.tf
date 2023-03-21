c8bcea497a14479850a3cc836af4370e59570e4b
AlexandrosSak <119606662+AlexandrosSak@users.noreply.github.com>
2023-03-21 13:07:32 +0200

    Delete TF-OCI directory

--- TF-OCI/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "oci_tenancy" {} 
-variable "oci_user" {}
-variable "oci_fingerprint" {}
-variable "oci_key" {}
-variable "oci_region" {}
-variable "oci_compartment" {}
-variable "private_key_file" {}
-variable "public_key" {}
-
