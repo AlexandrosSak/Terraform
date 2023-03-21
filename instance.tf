6798ac473f73c923387fdb7b86c17861fb8e65fe
SAKIOTIS Alexandros <asakiotis@netcompany-intrasoft.com>
2023-03-21 13:05:03 +0200

    updated Terraform files

---
+++ instance.tf
@@ -0,0 +1,62 @@
+resource "oci_core_instance" "TEST-Comp-Inst"  {
+    depends_on = [oci_file_storage_mount_target.my_mount_target_1]
+    count               =  2
+    compartment_id = var.oci_compartment
+    availability_domain   = "sgAm:EU-FRANKFURT-1-AD-${count.index + 1}"
+    shape = "VM.Standard.E2.4"      #"VM.Standard2.1"  #"VM.Standard.E2.2"
+    source_details {
+        source_id = "ocid1.image.oc1.eu-frankfurt-1.aaaaaaaa47555lp4mjbiuf64doxtnbimrwk57m4sfgu3gonaf5i2cteil5iq"
+        source_type = "image"
+  }
+
+    display_name          = "${var.display_name}-${count.index + 1}"
+    create_vnic_details {
+        assign_public_ip = false
+        subnet_id             = oci_core_subnet.Bastion_subnet[count.index].id
+ } 
+    metadata = {
+      user_data = base64encode(var.user-data)
+      ssh_authorized_keys = var.session_key_details_public_key_content
+  }
+
+    timeouts {
+       create = "60m"
+}
+    preserve_boot_volume = false
+    agent_config {
+    are_all_plugins_disabled = false
+    is_management_disabled = false
+    is_monitoring_disabled = false
+    plugins_config {
+    desired_state = "ENABLED" 
+    name = "Bastion"
+}
+}
+}
+variable "user-data" {
+  default = <<EOF
+#!/bin/bash -x
+echo '################### webserver userdata begins #####################'
+touch ~opc/userdata.`date +%s`.start
+# echo '########## yum update all ###############'
+# yum update -y
+echo '########## basic webserver ##############'
+yum install -y httpd
+systemctl enable  httpd.service
+systemctl start  httpd.service
+echo '<html><head></head><body><pre><code>' > /var/www/html/index.html
+hostname >> /var/www/html/index.html
+echo '' >> /var/www/html/index.html
+cat /etc/os-release >> /var/www/html/index.html
+echo '</code></pre></body></html>' >> /var/www/html/index.html
+firewall-offline-cmd --add-service=http
+systemctl enable  firewalld
+systemctl restart  firewalld
+touch ~opc/userdata.`date +%s`.finish
+echo '################### webserver userdata ends #######################'
+sleep 10
+sudo mkdir -p /mnt/nfs 
+sudo mount 10.0.2.210:/example_path /mnt/nfs
+
+EOF
+}
---
+++ loadbalancer2.tf
@@ -0,0 +1,84 @@
+# Create a private load balancer
+resource "oci_load_balancer_load_balancer" "private_lb" {
+  compartment_id = var.oci_compartment
+  display_name   = "private_lb"
+  shape = "flexible"
+  shape_details {
+  minimum_bandwidth_in_mbps = 10
+  maximum_bandwidth_in_mbps = 100
+}
+  subnet_ids     = [oci_core_subnet.Bastion_subnet[0].id]
+  is_private     = true
+
+}
+# Private Load Balancer Listener
+resource "oci_load_balancer_listener" "private_lb_listener" {
+  name              = "private_lb_listener"
+  default_backend_set_name =  oci_load_balancer_backend_set.private_lb_backend.name
+  protocol          = "TCP"
+  port              = 80  
+  load_balancer_id  = oci_load_balancer_load_balancer.private_lb.id
+}
+# Backend resources for private instances
+resource "oci_load_balancer_backend" "private_lb_backend_instance" {
+  count              = 2  
+  ip_address         = oci_core_instance.TEST-Comp-Inst[count.index].private_ip
+  load_balancer_id   = oci_load_balancer_load_balancer.private_lb.id
+  port               = 80 
+  backendset_name    = oci_load_balancer_backend_set.private_lb_backend.name 
+}
+# Backend set for private load balancer
+resource "oci_load_balancer_backend_set" "private_lb_backend" {
+  load_balancer_id  = oci_load_balancer_load_balancer.private_lb.id
+  name              = "private_lb_backend"
+  policy            = "ROUND_ROBIN"
+  health_checker {
+           protocol = "HTTP"
+           url_path = "/"
+               port = 80
+}
+}
+#data "oci_core_private_ip" "LB_private_ip" {
+#  private_ip_id = oci_load_balancer_load_balancer.private_lb.ip_address
+#}
+#Create a public load balancer
+#resource "oci_load_balancer_load_balancer" "public_lb" {
+#  compartment_id = var.oci_compartment
+#  display_name   = "public_nlb"
+#  subnet_ids = [oci_core_subnet.Public_subnet[0].id]
+#  shape = "flexible"
+#  is_private     = false
+#}
+
+# Public Load Balancer Backends
+#resource "oci_load_balancer_backend" "public_lb_backend" {
+#  ip_address = oci_load_balancer_load_balancer.private_lb.private_ips
+#  port              = 80
+#  load_balancer_id  = oci_load_balancer_load_balancer.public_lb.id
+#  backendset_name   = oci_load_balancer_backend_set.public_lb_backend_set.name #"public_lb_backend"
+#}
+
+# Public Load Balancer Listener
+#resource "oci_load_balancer_listener" "public_lb_listener" {
+#  name              = "public_listener"
+#  default_backend_set_name = "public_backend_set" #oci_load_balancer_backend_set.public_lb_backend_set.name
+#  protocol          = "HTTP"
+#  port              = 80
+#  load_balancer_id  = oci_load_balancer_load_balancer.public_lb.id
+#}
+
+
+#Public backend set
+#resource "oci_load_balancer_backend_set" "public_lb_backend_set" {
+#  load_balancer_id = oci_load_balancer_load_balancer.public_lb.id
+#  name = "public_lb_backend"
+#  policy = "ROUND_ROBIN"
+#  health_checker {
+#  protocol = "HTTP"
+#  url_path = "/"
+#  port = 80
+#  return_code = 200
+#  retries = 3
+#  timeout_in_millis = 3000
+#}
+#}
