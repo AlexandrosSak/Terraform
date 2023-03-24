573689d6d0e9c1c28bba0b5d90df11030d2d9bae
SAKIOTIS Alexandros <asakiotis@netcompany-intrasoft.com>
2023-03-24 16:18:55 +0200

    Minor changes

--- bastion.tf
+++ bastion.tf
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-resource "time_sleep" "wait_3_minutes_for_bastion_plugin" {
-  depends_on      = [oci_core_instance.TEST-Comp-Inst[0], oci_core_instance.TEST-Comp-Inst[1]]
-  create_duration = "4m"
-}
-
-resource "oci_bastion_bastion" "test_bastion" {
-  bastion_type     = "STANDARD"
-  compartment_id   = var.oci_compartment
-  target_subnet_id = oci_core_subnet.Bastion_subnet[0].id
-  #target_subnet_id          = oci_core_subnet.Bastion_subnet[count.index].id      
-  name                         = var.bastion_name #"${var.bastion_name}_${count.index + 1}"
-  freeform_tags                = var.bastion_freeform_tags
-  client_cidr_block_allow_list = var.bastion_client_cidr_block_allow_list
-  max_session_ttl_in_seconds   = var.bastion_max_session_ttl_in_seconds
-}
-=======
 #Add waiting time for Bastion plugin to be downloaded and installed
 resource "time_sleep" "wait_3_minutes_for_bastion_plugin" {
     depends_on     = [oci_core_instance.TEST-Comp-Inst[0], oci_core_instance.TEST-Comp-Inst[1]]
@@ -32,7 +15,6 @@ resource "oci_bastion_bastion" "test_bastion" {
   max_session_ttl_in_seconds = var.bastion_max_session_ttl_in_seconds
 }
 #Add managed ssh on both instances
->>>>>>> d851fa01c3dbc97efcf807418c3a7ec0228e0ae6
 resource "oci_bastion_session" "test_session_managed_ssh" {
   count      = 2
   bastion_id = oci_bastion_bastion.test_bastion.id
@@ -41,19 +23,11 @@ resource "oci_bastion_session" "test_session_managed_ssh" {
   }
 
   target_resource_details {
-<<<<<<< HEAD
-    session_type                               = var.session_target_resource_details_session_type_managed_ssh
-    target_resource_id                         = oci_core_instance.TEST-Comp-Inst[count.index].id
-    target_resource_operating_system_user_name = "opc"
-    target_resource_port                       = var.session_target_resource_details_target_resource_port
-    target_resource_private_ip_address         = oci_core_instance.TEST-Comp-Inst[count.index].private_ip
-=======
     session_type                = var.session_target_resource_details_session_type_managed_ssh
     target_resource_id = oci_core_instance.TEST-Comp-Inst[count.index].id
     target_resource_operating_system_user_name = "opc"
     target_resource_port        = var.session_target_resource_details_target_resource_port
     target_resource_private_ip_address = oci_core_instance.TEST-Comp-Inst[count.index].private_ip
->>>>>>> d851fa01c3dbc97efcf807418c3a7ec0228e0ae6
   }
 
   display_name           = var.session_display_name
