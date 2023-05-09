ed72fd57ce9f97da48dd4e18377a9b7b035ca715
Alexandros Sakiotis <119606662+AlexandrosSak@users.noreply.github.com>
2023-05-09 16:09:15 +0300

    Update network.tf

--- network.tf
+++ network.tf
@@ -1,16 +1,16 @@
 provider "oci" {
-  tenancy_ocid = var.oci_tenancy
-  user_ocid = var.oci_user
-  fingerprint = var.oci_fingerprint
+  tenancy_ocid     = var.oci_tenancy
+  user_ocid        = var.oci_user
+  fingerprint      = var.oci_fingerprint
   private_key_path = var.oci_key
-  region = var.oci_region
+  region           = var.oci_region
 }
 
 data "oci_core_services" "test_services" {
 filter {
-  name = "name"
+  name   = "name"
   values = ["All .* Services In Oracle Services Network"]
-  regex = true
+  regex  = true
 }
 }
 output "services" {
@@ -18,10 +18,10 @@ output "services" {
 }
 
 resource "oci_core_vcn" "my_vcn" {
-  cidr_block = "10.0.0.0/16"
-  display_name = "my_vcn"
-  dns_label = "examplevcn"
-  compartment_id = var.oci_compartment
+  cidr_block      = "10.0.0.0/16"
+  display_name    = "my_vcn"
+  dns_label       = "examplevcn"
+  compartment_id  = var.oci_compartment
 }
 
 resource "oci_core_security_list" "terraformsecuritylist" {
@@ -72,10 +72,10 @@ resource "oci_core_default_route_table" "default_route_table" {
   manage_default_resource_id = oci_core_vcn.my_vcn.default_route_table_id
   display_name               = "BastionRouteTable"
 route_rules {
-  destination = "0.0.0.0/0"  #data.oci_core_services.test_services.services[0]["cidr_block"]
-  destination_type = "CIDR_BLOCK"
-  #network_entity_id = oci_core_service_gateway.example.id
-   network_entity_id = oci_core_nat_gateway.nat_gateway.id
+  destination                = "0.0.0.0/0"  #data.oci_core_services.test_services.services[0]["cidr_block"]
+  destination_type           = "CIDR_BLOCK"
+  #network_entity_id         = oci_core_service_gateway.example.id
+   network_entity_id         = oci_core_nat_gateway.nat_gateway.id
 }
 }
 #Private Subnet
@@ -97,13 +97,13 @@ resource "oci_core_subnet" "Bastion_subnet" {
 #public subnet for public lb
 resource "oci_core_subnet" "Public_subnet" {
   count = 2
-  availability_domain = "sgAm:EU-FRANKFURT-1-AD-${count.index + 1}"
-  cidr_block            = count.index == 0 ? "10.0.128.0/24" : "10.0.129.0/24"
-  display_name    = "pub_sub${count.index + 1}"
+  availability_domain        = "sgAm:EU-FRANKFURT-1-AD-${count.index + 1}"
+  cidr_block                 = count.index == 0 ? "10.0.128.0/24" : "10.0.129.0/24"
+  display_name               = "pub_sub${count.index + 1}"
   prohibit_public_ip_on_vnic = false
-  vcn_id          = oci_core_vcn.my_vcn.id
-  compartment_id = var.oci_compartment
-  security_list_ids   = [oci_core_security_list.terraformsecuritylist.id]
-  route_table_id      = oci_core_vcn.my_vcn.default_route_table_id
-  dhcp_options_id     = oci_core_vcn.my_vcn.default_dhcp_options_id
+  vcn_id                     = oci_core_vcn.my_vcn.id
+  compartment_id             = var.oci_compartment
+  security_list_ids          = [oci_core_security_list.terraformsecuritylist.id]
+  route_table_id             = oci_core_vcn.my_vcn.default_route_table_id
+  dhcp_options_id            = oci_core_vcn.my_vcn.default_dhcp_options_id
 }
