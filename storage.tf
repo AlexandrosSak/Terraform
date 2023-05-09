6e8b4fa4d587d3acb5f9fea89d21225c0bf7396b
Alexandros Sakiotis <119606662+AlexandrosSak@users.noreply.github.com>
2023-05-09 16:10:55 +0300

    Update storage.tf

--- storage.tf
+++ storage.tf
@@ -1,31 +1,31 @@
 #####Create a block volume in OCI:
 resource "oci_core_volume" "example_block_volume" {
-  availability_domain = "sgAm:EU-FRANKFURT-1-AD-1"
-  compartment_id = var.oci_compartment
-  display_name        = "example_block_volume"
-  size_in_gbs         = "50"
+  availability_domain  = "sgAm:EU-FRANKFURT-1-AD-1"
+  compartment_id       = var.oci_compartment
+  display_name         = "example_block_volume"
+  size_in_gbs          = "50"
 }
 
 ####### Create an NFS file system :
 resource "oci_file_storage_file_system" "example_file_system" {
-  availability_domain = "sgAm:EU-FRANKFURT-1-AD-1"
-  compartment_id = var.oci_compartment
-  display_name        = "example_file_system"
+  availability_domain   = "sgAm:EU-FRANKFURT-1-AD-1"
+  compartment_id        = var.oci_compartment
+  display_name          = "example_file_system"
 }
 ####===================Mount Target=======================================
 resource "oci_file_storage_mount_target" "my_mount_target_1" {
     #Required
-    availability_domain = "sgAm:EU-FRANKFURT-1-AD-1"
-    compartment_id = var.oci_compartment
-    subnet_id = oci_core_subnet.Bastion_subnet[0].id
-    display_name = "my-mount-target-1"
-    ip_address = "10.0.2.210"
+    availability_domain  = "sgAm:EU-FRANKFURT-1-AD-1"
+    compartment_id       = var.oci_compartment
+    subnet_id            = oci_core_subnet.Bastion_subnet[0].id
+    display_name         = "my-mount-target-1"
+    ip_address           = "10.0.2.210"
 }
 ####==================Export_set=============================================
 
 resource "oci_file_storage_export_set" "my_export_set_1" {
     #Required
-    mount_target_id = oci_file_storage_mount_target.my_mount_target_1.id
+    mount_target_id     = oci_file_storage_mount_target.my_mount_target_1.id
     max_fs_stat_bytes   = "53687091200"
     max_fs_stat_files   = "53687091200"
 }
@@ -33,26 +33,26 @@ resource "oci_file_storage_export_set" "my_export_set_1" {
 ###### Attach the block volume to an NFS file system:
 resource "oci_file_storage_export" "example_export" {
   export_set_id   = oci_file_storage_export_set.my_export_set_1.id
-  file_system_id = oci_file_storage_file_system.example_file_system.id
+  file_system_id  = oci_file_storage_file_system.example_file_system.id
   path            = "/example_path"
 }
 resource "oci_core_volume_attachment" "example_attachment" {
  # availability_domain = "sgAm:EU-FRANKFURT-1-AD-1"
-  instance_id         = "${oci_core_instance.TEST-Comp-Inst[0].id}"
-  volume_id           = "${oci_core_volume.example_block_volume.id}"
-  attachment_type     = "paravirtualized"
+  instance_id          = "${oci_core_instance.TEST-Comp-Inst[0].id}"
+  volume_id            = "${oci_core_volume.example_block_volume.id}"
+  attachment_type      = "paravirtualized"
 }
 
 
 ######## Mount the NFS file system on the instance===============================================
 
 output "nfs_priv_ip" {
-  value = oci_file_storage_mount_target.my_mount_target_1.ip_address #oci_file_storage_mount_target.my_mount_target_1.private_ip_ids[0]
+  value               = oci_file_storage_mount_target.my_mount_target_1.ip_address #oci_file_storage_mount_target.my_mount_target_1.private_ip_ids[0]
 }
 
 
 resource "null_resource" "mount_nfs" {
-   depends_on = [oci_file_storage_mount_target.my_mount_target_1]
+   depends_on         = [oci_file_storage_mount_target.my_mount_target_1]
 
 # provisioner "remote-exec" {
    # inline = [
